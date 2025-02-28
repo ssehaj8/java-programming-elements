86ca98f1e40d1226943e49685eef75d30fe8bf95
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-03-01 00:03:12 +0530

    [Sehajpreet] Add: Covert Kilometers to Miles

---
+++ KmToMiles.java
@@ -0,0 +1,11 @@
+public class KmToMiles {
+    public static void main(String[] args) {
+        // Given distance in kilometers
+         double kilometers = 10.8;
+
+        // Conversion 
+        double miles =kilometers / 1.6; 
+
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
